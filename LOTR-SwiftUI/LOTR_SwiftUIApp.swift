ffdbb1ab901fe455b074a86d829520590c198f78
Raja Adeel Ahmed <adeel.ahmed@telenorbank.pk>
2023-03-28 17:05:39 +0500

    Initial Commit

---
+++ LOTR-SwiftUI/LOTR_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_SwiftUIApp.swift
+//  LOTR-SwiftUI
+//
+//  Created by Raja Adeel Ahmed on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
